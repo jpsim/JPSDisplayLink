bc349213479b0c98d6f09026e0e9a29775bf05f9
JP Simard <jp@jpsim.com>
2014-01-13 21:29:58 -0500

    Initial Commit

---
+++ JPSDisplayLinkDemo/JPSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JPSAppDelegate.h
+//  JPSDisplayLinkDemo
+//
+//  Created by JP Simard on 1/13/2014.
+//  Copyright (c) 2014 JP Simard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JPSDisplayLinkDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JPSDisplayLinkDemo
+//
+//  Created by JP Simard on 1/13/2014.
+//  Copyright (c) 2014 JP Simard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JPSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JPSAppDelegate class]));
+    }
+}
